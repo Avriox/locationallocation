e735111e8414806f8978725803e635be65fb428b
Giacomo Falchetta <36954873+giacfalk@users.noreply.github.com>
2025-04-27 09:45:54 +0200

    Update mask_raster_to_polygon.R

--- R/mask_raster_to_polygon.R
+++ R/mask_raster_to_polygon.R
@@ -8,49 +8,35 @@
 #' @keywords masking
 #' @export
 
-mask_raster_to_polygon <- function(ras = NULL, mask = NULL, inverse = FALSE, updatevalue = NA) {
-
+mask_raster_to_polygon <- function (ras = NULL, mask = NULL, inverse = FALSE, updatevalue = NA) 
+{
   stopifnot(inherits(ras, "Raster"))
-
   stopifnot(inherits(mask, "Raster") | inherits(mask, "sf"))
-
-  ## If mask is a polygon sf, pre-process:
-
+  
+  mask <- sf::st_transform(mask, 4326)
+  ras <- raster::projectRaster(ras, crs=sf::st_crs(mask)$proj4string)
+  
   if (inherits(mask, "sf")) {
-
-    stopifnot(unique(as.character(sf::st_geometry_type(mask))) %in% c("POLYGON", "MULTIPOLYGON"))
-
-    # First, crop sf to raster extent
-    sf.crop <- suppressWarnings(sf::st_crop(mask,
-                                            y = c(
-                                              xmin = raster::xmin(ras),
-                                              ymin = raster::ymin(ras),
-                                              xmax = raster::xmax(ras),
-                                              ymax = raster::ymax(ras)
-                                            )))
+    stopifnot(unique(as.character(sf::st_geometry_type(mask))) %in% 
+                c("POLYGON", "MULTIPOLYGON"))
+    sf.crop <- suppressWarnings(sf::st_crop(mask, y = c(xmin = raster::xmin(ras), 
+                                                        ymin = raster::ymin(ras), xmax = raster::xmax(ras), 
+                                                        ymax = raster::ymax(ras))))
     sf.crop <- sf::st_cast(sf.crop)
-
-    # Now rasterize sf
     mask <- fasterize::fasterize(sf.crop, raster = ras)
-
   }
-
-
-
   if (isTRUE(inverse)) {
-
-    ras.masked <- raster::overlay(ras, mask,
-                                  fun = function(x, y)
-                                  {ifelse(!is.na(y), updatevalue, x)})
-
-  } else {
-
-    ras.masked <- raster::overlay(ras, mask,
-                                  fun = function(x, y)
-                                  {ifelse(is.na(y), updatevalue, x)})
-
+    ras.masked <- raster::overlay(ras, mask, fun = function(x, 
+                                                            y) {
+      ifelse(!is.na(y), updatevalue, x)
+    })
   }
-
+  else {
+    ras.masked <- raster::overlay(ras, mask, fun = function(x, 
+                                                            y) {
+      ifelse(is.na(y), updatevalue, x)
+    })
+  }
   ras.masked
-
 }
+
